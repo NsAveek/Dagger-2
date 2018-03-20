eff1dd90821941354d15c32bf9ddfe6f02beadbb
Aveek <nsaveek@gmail.com>
2018-03-20 17:27:08 +0800

    Component updated

--- app/src/main/java/uwanttolearn/dagger2/kotlin/home/KotlinHomeActivityComponent.kt
+++ app/src/main/java/uwanttolearn/dagger2/kotlin/home/KotlinHomeActivityComponent.kt
@@ -1,6 +1,12 @@
 package uwanttolearn.dagger2.kotlin.home
 
+import android.support.v7.widget.RecyclerView
+import android.widget.ProgressBar
+import com.bumptech.glide.Glide
 import dagger.Component
+import io.reactivex.disposables.Disposable
+import uwanttolearn.dagger2.java.home.adapter.HomeAdapter
+import uwanttolearn.dagger2.java.repositories.github.GitHubRepository
 
 /**
  * Created by Aveek on 20/03/2018.
@@ -9,14 +15,12 @@ import dagger.Component
 @Component( modules = arrayOf(KotlinHomeActivityModule::class))
 interface KotlinHomeActivityComponent {
 
-    
-
-//    RecyclerView getRecyclerView()
-//    ProgressBar getProgressBar()
-//    GithubRepository getGithubRepository()
-//    HomeAdapter getHomeAdapter
-//    RecyclerView.LayoutManager getLayoutManager()
-//    Disposable getDisposable()
-//    Glide getGlide()
+    fun getRecyclerView()   : RecyclerView
+    fun getProgressBar()    : ProgressBar
+    fun getGithubRepository()   : GitHubRepository
+    fun getHomeAdapter() : HomeAdapter
+    fun getLayoutManager() : RecyclerView.LayoutManager
+    fun getDisposable() : Disposable
+    fun getGlide()  :   Glide
 
 }
